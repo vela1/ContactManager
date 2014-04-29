0fffa2a339e7e3d1d4d6309e6c15435e54a26fad
topuzb <topuzb@AP086164.hrw.hs-weingarten.de>
2014-04-29 15:04:21 +0200

    init repo

---
+++ cm.ContactManager/src/main/java/de/sit/cm/App.java
@@ -0,0 +1,13 @@
+package de.sit.cm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
